5528792ad15f6d0c98978664143b7ba8b8b55d94
David Mashburn <david.n.mashburn@gmail.com>
2015-08-19 14:13:22 -0400

    Add package structure

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+# Read the version number
+with open("wxPyGameVideoPlayer/_version.py") as f:
+    exec(f.read())
+
+setup(
+    name='wxPyGameVideoPlayer',
+    version=__version__, # use the same version that's in _version.py
+    author='David N. Mashburn',
+    author_email='david.n.mashburn@gmail.com',
+    packages=['wxPyGameVideoPlayer'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/wxpygamevideoplayer/',
+    license='LICENSE.txt',
+    description='Just a simple video player using wxPython, PyGame, and OpenCV',
+    long_description=open('README.md').read(),
+    install_requires=[
+                      'attrdict>=2.0',
+                      'numpy>=1.0',
+                      'wxPython>=2.8',
+                      'matplotlib>=1.0'
+                      'pygame>=1.9',
+                      # 'cv2', # there is no way to depend on this, but you need to have it :)
+                     ],
+)
---
+++ wxPyGameVideoPlayer/__init__.py
@@ -0,0 +1,7 @@
+import cv2_utils
+import wx_func_utils
+import wx_video_ui
+import pygame_interface
+import opencv_player
+import _version
+from _version import *
